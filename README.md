00d02e450f73ad34027811152ad0998093abfdda
Roller Davic <rollerdavic@gmail.com>
2018-11-04 08:54:17 +0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# roller-api
+# Roller API
+api.roller.today is providing this api for public us as desired at the following url
+https://api.roller.today/?wallet=0x11905bd0863ba579023f662d1935e39d0c671933
+
+json output would be
+{
+"wallet":"0x11905bd0863ba579023f662d1935e39d0c671933",
+"balance":"1119.8800567580"
+}
+
+just replace that wallet with your own.
+
+feel free to add pull requests, or fork for your uwn usage
+
+ideas this can help with for instance include
+scripting daily balance reports
+ok, other things :)
+this would also work for nearly any ethash based coin, just change the port to the coins rpc port.
+
+How to run this?
+install php and php-curl if needed. you can then run it with command line.
+if you want to have it serve up by a webserver, put the files into the web servers root directory and try the format above
+
+you can use https://node.roller.today for the rpc address or run a pirl node locally and set it to localhost:8545 for local rpc calls
+
+
+:)
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+//created by phatblinkie to help with a simple query for scripts to access
+error_reporting(0);
+
+//pass some simple sanity checks
+if ( $_REQUEST['wallet'] == "" ) {echo "url should be in format http://api.roller.today/?wallet=0xasdfjasdlkjasdflkj"; exit;}
+if ( strlen($_REQUEST['wallet']) != "42" ) { echo "wallet should be 42 char, including the 0x beginning"; exit;}
+
+//include ethereum php library
+require 'ethereum-php/ethereum.php';
+//create object
+//pirls official rpc server, made for things like this
+$ethc = new Ethereum('https://node.roller.today/', '80');
+
+//use this if your running a local pirl node (be sure to start it up with --rpc after the command)
+#$ethc = new Ethereum('127.0.0.1', 8545);
+
+//if passed, capture wallet id
+$addr = $_REQUEST['wallet'];
+
+//get balance
+$dec = $ethc->eth_getBalance($addr, "latest");
+
+//convert from hex to dev, then to human type numbers
+// 10 decimal spots, with a period, no thousands separator  = 1119.8800567580
+
+$roller = number_format((hexdec($dec)/1000000000000000000), 10, ".", "");
+
+//setup array for json encoding
+$assocArray = array();
+$assocArray['wallet'] = ''.$addr.'';
+$assocArray['balance'] = ''.$roller.'';
+
+//print_r($assocArray);
+
+//encode in json format
+$jsondata = json_encode($assocArray);
+
+//finally, echo result of the work.
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json;charset=utf-8');
+echo $jsondata;
+?>
